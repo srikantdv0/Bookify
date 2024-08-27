6e06b150f505836e89e0190b466cac836863f514
Srikant <srikantdv0@gmail.com>
2024-08-27 23:39:56 +0530

    27Aug

---
+++ src/Bookify.Domain/Apartments/Currency.cs
@@ -0,0 +1,21 @@
+namespace Bookify.Domain.Apartments;
+
+public record Currency
+{
+    public static readonly Currency Usd = new("USD");
+    public static readonly Currency Eur = new("EUR");
+
+    public Currency(string code) => Code = code;
+    public string Code {get; init;}
+    public static Currency FormCode(string code)
+    {
+        return All.FirstOrDefault(c => c.Code == code) ??
+                throw new ApplicationException("The currency code is invalid");
+    }
+    public static readonly IReadOnlyCollection<Currency> All = new[]
+    {
+        Usd,
+        Eur
+    };
+
+};
