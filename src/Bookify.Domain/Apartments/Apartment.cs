d7bccc044f46654ce709a328edd21ea52afc8976
Srikant <srikantdv0@gmail.com>
2024-08-28 07:28:50 +0530

    2808M

--- src/Bookify.Domain/Apartments/Apartment.cs
+++ src/Bookify.Domain/Apartments/Apartment.cs
@@ -3,16 +3,28 @@ namespace Bookify.Domain.Apartments;
 
 public sealed class Apartments : Entity
 {
-    public Apartments(Guid id) : base(id)
+    public Apartments(
+        Guid id,
+        Name name,
+        Description description,
+        Address address,
+        Money price,
+        Money cleaningfee,
+        List<Amenity> amenities
+        ) : base(id)
     {
+        Name = name;
+        Description = description;
+        Address = address;
+        Price = price;
+        CleaningFee = cleaningfee;
+        Amenities = amenities;
     }
     public Name Name {get;private set;}
     public Description Description {get;private set;}
     public Address Address {get; private set;}
-    public decimal PriceAmount {get;private set;}
-    public string PriceCurrency {get;private set;}
-    public decimal CleaningFeeAmount {get;private set;}
-    public string CleaningFeeCurrency {get;private set;}
+    public Money Price {get; private set;}
+    public Money CleaningFee {get; private set;}
     public DateTime? LastBookedOnUTC {get;private set;}
     public List<Amenity> Amenities{get;private set;} = new();
 
--- src/Bookify.Domain/Apartments/Currency.cs
+++ src/Bookify.Domain/Apartments/Currency.cs
@@ -4,6 +4,7 @@ public record Currency
 {
     public static readonly Currency Usd = new("USD");
     public static readonly Currency Eur = new("EUR");
+    internal static readonly Currency None = new("");
 
     public Currency(string code) => Code = code;
     public string Code {get; init;}
@@ -15,7 +16,7 @@ public record Currency
     public static readonly IReadOnlyCollection<Currency> All = new[]
     {
         Usd,
-        Eur
+        Eur,
     };
 
 };
