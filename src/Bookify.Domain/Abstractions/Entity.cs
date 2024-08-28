5a13115a01c6f94c5cb558b7c49f50768784b97b
Srikant <srikantdv0@gmail.com>
2024-08-28 23:14:16 +0530

    2808E

--- src/Bookify.Domain/Abstractions/Entity.cs
+++ src/Bookify.Domain/Abstractions/Entity.cs
@@ -2,9 +2,25 @@ namespace Bookify.Domain.Abstractions;
 
 public abstract class Entity
 {
+    private readonly List<IDomainEvent> _domainEvents = new();
     protected Entity(Guid id)
     {
         Id = id;
     }
    public Guid Id {get; init; }
+
+   public IReadOnlyCollection<IDomainEvent> GetDomainEvents()
+   {
+        return _domainEvents.ToList();
+   }
+
+   public void ClearDomainEvents()
+   {
+        _domainEvents.Clear();
+   }
+
+   protected void RaiseDomainEvent(IDomainEvent domainEvent)
+   {
+        _domainEvents.Add(domainEvent);
+   }
 }
