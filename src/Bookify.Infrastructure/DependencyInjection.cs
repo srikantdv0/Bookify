682e242794c468b29ab0f83cab91f8da5e74328c
Srikant <srikantdv0@gmail.com>
2024-09-19 08:37:22 +0530

    1909M

--- src/Bookify.Infrastructure/DependencyInjection.cs
+++ src/Bookify.Infrastructure/DependencyInjection.cs
@@ -4,6 +4,7 @@ using Bookify.Application.Abstractions.Clock;
 using Bookify.Infrastructure.Clock;
 using Bookify.Application.Abstractions.Email;
 using Bookify.Infrastructure.Email;
+using Microsoft.EntityFrameworkCore;
 
 namespace Bookify.Infrastructure;
 
@@ -15,6 +16,13 @@ public static class DependencyInjection
     {
         services.AddTransient<IDateTimeProvider, DateTimeProvider>();
         services.AddTransient<IEmailService, EmailService>();
+
+        var connectionString = configuration.GetConnectionString("Database") ??
+                                throw new ArgumentNullException(nameof(configuration));
+        services.AddDbContext<ApplicationDbContext>(options => 
+        {
+            options.UseNpgsql(connectionString).UseSnakeCaseNamingConvention();
+        });
         return services;
     }
 }
